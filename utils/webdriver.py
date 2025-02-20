0d0c75414a5c5374467366fb261ca5df128a01ff
evan-digiteer <marc@digiteer.digital>
2025-02-20 20:42:01 +0800

    Add options to disable WebRTC in Chrome driver setup

--- utils/webdriver.py
+++ utils/webdriver.py
@@ -8,6 +8,10 @@ def setup_driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    # Add these new options to disable WebRTC
+    chrome_options.add_argument('--disable-webrtc')
+    chrome_options.add_argument('--disable-features=WebRTC')
+    chrome_options.add_argument('--disable-features=WebRTCStunProbing')
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
